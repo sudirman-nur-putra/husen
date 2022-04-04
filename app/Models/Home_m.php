5f183228e7de0e8819bcf347bb11160b1cae00c9
sudirman-nur-putra <72235549+sudirman-nur-putra@users.noreply.github.com>
2022-04-05 06:10:37 +0700

    Add files via upload
    Models

---
+++ app/Models/Home_m.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Home_m{
+    protected $table = 'pemasukan';
+    protected $table = 'pengeluaran';
+    protected $table = 'penjualan';
+} 
---
+++ app/Models/Login_m.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Login_m{
+    protected $table = 'user';
+
+    public function getData($email, $password){
+        $db      = \Config\Database::connect();
+        $result  = $db->query("SELECT * FROM user WHERE email = '$email' AND password = '$password'");
+        return $result->getResultArray();
+    }
+
+    public function getDatae($email){
+        $db      = \Config\Database::connect();
+        $result  = $db->query("SELECT * FROM user WHERE email = '$email'");
+        return $result->getResultArray();
+    }
+
+    public function simpan($nama, $tanggal, $no, $email, $pass){
+        $db      = \Config\Database::connect();
+        $result  = $db->query("INSERT INTO user (nama, tanggal_lahir, no_hp, email, password) VALUES ('$nama','$tanggal', '$no','$email','$pass')");
+        return $result;
+    }
+} 
