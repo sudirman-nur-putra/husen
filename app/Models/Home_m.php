8324712feab4fab1657a819325941c297d8afad6
sudirman-nur-putra <72235549+sudirman-nur-putra@users.noreply.github.com>
2022-04-12 09:25:33 +0700

    Update Home_m.php

--- app/Models/Home_m.php
+++ app/Models/Home_m.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class Home_m{
-    protected $table = 'pemasukan';
-    protected $table = 'pengeluaran';
-    protected $table = 'penjualan';
+     $db      = \Config\Database::connect();
+    
+    public function getJumlahPendapatan(){
+        $result  = $db->query("SELECT count(total_pendapatan) FROM pendapatan_harian");
+        return $result->getResultArray();
+    }
+
+    public function getJumlahBarang(){
+        $result  = $db->query("SELECT count(jml_barang) FROM transaksi");
+        return $result->getResultArray();
+    }
+
+    public function getPendapatanHarian(){
+        $result  = $db->query("SELECT tanggal, total_pendapatan FROM pendapatan_harian");
+        return $result->getResultArray(); 
+    }
 } 
