a0562b9c1d42c0b0201001b8d518400869d35fe9
bimantoroar <75794187+bimantoroar@users.noreply.github.com>
2022-04-19 11:13:07 +0700

    update db_husen.sql

--- db_husen.sql
+++ db_husen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2022 at 04:56 AM
+-- Generation Time: Apr 19, 2022 at 06:12 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -18,8 +18,20 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_husen`
+-- Database: `db_husein`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun`
+--
+
+CREATE TABLE `akun` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -29,28 +41,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `barang` (
   `id` int(11) NOT NULL,
-  `nama_barang` text NOT NULL,
-  `jumlah_barang` int(11) NOT NULL,
-  `harga_barang` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `barang`
---
-
-INSERT INTO `barang` (`id`, `nama_barang`, `jumlah_barang`, `harga_barang`) VALUES
-(1, 'Knalpot', 2, 500000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `dropshipper`
---
-
-CREATE TABLE `dropshipper` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `no_hp` varchar(100) NOT NULL
+  `nama` varchar(255) NOT NULL,
+  `harga_beli` int(100) NOT NULL,
+  `harga_jual_dropshipper` int(100) NOT NULL,
+  `harga_jual_reseller` int(100) NOT NULL,
+  `stok` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -61,8 +56,32 @@ CREATE TABLE `dropshipper` (
 
 CREATE TABLE `gaji` (
   `id` int(11) NOT NULL,
-  `id_dropshipper` int(11) NOT NULL,
-  `nominal` int(11) NOT NULL
+  `id_user` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nominal` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keuntungan_dropshipper`
+--
+
+CREATE TABLE `keuntungan_dropshipper` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `total_keuntungan` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE `level` (
+  `id` int(11) NOT NULL,
+  `level` enum('Dropshipper','Reseller','Admin') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -73,84 +92,24 @@ CREATE TABLE `gaji` (
 
 CREATE TABLE `overhead` (
   `id` int(11) NOT NULL,
-  `nominal` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `packing`
---
-
-CREATE TABLE `packing` (
-  `id` int(11) NOT NULL,
   `tanggal` date NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `packing`
---
-
-INSERT INTO `packing` (`id`, `tanggal`, `nama`, `harga`) VALUES
-(1, '2022-03-24', 'biman', 5000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pemasukan`
---
-
-CREATE TABLE `pemasukan` (
-  `id` int(11) NOT NULL,
-  `id_dropshipper` int(11) NOT NULL,
-  `id_reseller` int(11) NOT NULL
+  `keterangan` enum('TRANSPORT','PERALATAN PACKING') NOT NULL,
+  `jumlah_pengeluaran` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pembelian`
+-- Table structure for table `pembelian_barang`
 --
 
-CREATE TABLE `pembelian` (
-  `id` int(11) NOT NULL,
-  `id_barang` int(11) NOT NULL,
-  `id_packing` int(11) NOT NULL,
-  `nominal` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `pembelian`
---
-
-INSERT INTO `pembelian` (`id`, `id_barang`, `id_packing`, `nominal`) VALUES
-(1, 1, 1, 1005000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pengeluaran`
---
-
-CREATE TABLE `pengeluaran` (
-  `id` int(11) NOT NULL,
-  `id_pembelian` int(11) NOT NULL,
-  `id_gaji` int(11) NOT NULL,
-  `id_overhead` int(11) NOT NULL,
-  `total_pengeluaran` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reseller`
---
-
-CREATE TABLE `reseller` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL
+CREATE TABLE `pembelian_barang` (
+  `id` int(100) NOT NULL,
+  `id_barang` int(100) NOT NULL,
+  `nama_toko` varchar(255) NOT NULL,
+  `jumlah_beli` int(100) NOT NULL,
+  `total_harga` int(100) NOT NULL,
+  `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -161,12 +120,17 @@ CREATE TABLE `reseller` (
 
 CREATE TABLE `transaksi_dropshipper` (
   `id` int(11) NOT NULL,
-  `id_dropshipper` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `id_barang` int(11) NOT NULL,
-  `nama_pembeli` varchar(100) NOT NULL,
-  `harga_jual` int(11) NOT NULL,
   `tanggal` date NOT NULL,
-  `marketplace` enum('Lazada','Shopee') NOT NULL
+  `modal` int(100) NOT NULL,
+  `jumlah_barang` int(100) NOT NULL,
+  `harga_jual` int(100) NOT NULL,
+  `no resi` varchar(255) NOT NULL,
+  `status_packing` enum('Sendiri','Dari Husein') NOT NULL,
+  `marketplace` enum('Lazada','Shopee') NOT NULL,
+  `status` enum('Hilang','Sampai','Pengembalian') NOT NULL,
+  `keuntungan` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -177,11 +141,11 @@ CREATE TABLE `transaksi_dropshipper` (
 
 CREATE TABLE `transaksi_reseller` (
   `id` int(11) NOT NULL,
-  `id_reseller` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `id_barang` int(11) NOT NULL,
-  `jumlah_produk` int(11) NOT NULL,
-  `harga_produk` int(11) NOT NULL,
-  `tanggal` date NOT NULL
+  `tanggal` date NOT NULL,
+  `jumlah_barang` int(100) NOT NULL,
+  `total_pembelian` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -192,13 +156,20 @@ CREATE TABLE `transaksi_reseller` (
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `nama` varchar(255) NOT NULL,
+  `nomor_hp` varchar(255) NOT NULL,
+  `id_level` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `barang`
@@ -207,17 +178,24 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `dropshipper`
---
-ALTER TABLE `dropshipper`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `gaji`
 --
 ALTER TABLE `gaji`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `gajidrop` (`id_dropshipper`);
+  ADD KEY `gajianuser` (`id_user`);
+
+--
+-- Indexes for table `keuntungan_dropshipper`
+--
+ALTER TABLE `keuntungan_dropshipper`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`id_user`);
+
+--
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `overhead`
@@ -226,78 +204,49 @@ ALTER TABLE `overhead`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `packing`
+-- Indexes for table `pembelian_barang`
 --
-ALTER TABLE `packing`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pemasukan`
---
-ALTER TABLE `pemasukan`
+ALTER TABLE `pembelian_barang`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fdropshipper` (`id_dropshipper`),
-  ADD KEY `freseller` (`id_reseller`);
-
---
--- Indexes for table `pembelian`
---
-ALTER TABLE `pembelian`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `packing` (`id_packing`),
-  ADD KEY `id_barang` (`id_barang`);
-
---
--- Indexes for table `pengeluaran`
---
-ALTER TABLE `pengeluaran`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `pembelian` (`id_pembelian`),
-  ADD KEY `gaji` (`id_gaji`),
-  ADD KEY `overhead` (`id_overhead`);
-
---
--- Indexes for table `reseller`
---
-ALTER TABLE `reseller`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `barang` (`id_barang`);
 
 --
 -- Indexes for table `transaksi_dropshipper`
 --
 ALTER TABLE `transaksi_dropshipper`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `dropshipper` (`id_dropshipper`),
-  ADD KEY `barang` (`id_barang`);
+  ADD KEY `tduser` (`id_user`),
+  ADD KEY `tdbarang` (`id_barang`);
 
 --
 -- Indexes for table `transaksi_reseller`
 --
 ALTER TABLE `transaksi_reseller`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `reseller` (`id_reseller`),
-  ADD KEY `fbarang` (`id_barang`);
+  ADD KEY `tuser` (`id_user`),
+  ADD KEY `tbarang` (`id_barang`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `level` (`id_level`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `dropshipper`
---
-ALTER TABLE `dropshipper`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -307,40 +256,28 @@ ALTER TABLE `gaji`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `keuntungan_dropshipper`
+--
+ALTER TABLE `keuntungan_dropshipper`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `level`
+--
+ALTER TABLE `level`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `overhead`
 --
 ALTER TABLE `overhead`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `packing`
+-- AUTO_INCREMENT for table `pembelian_barang`
 --
-ALTER TABLE `packing`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `pemasukan`
---
-ALTER TABLE `pemasukan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `pembelian`
---
-ALTER TABLE `pembelian`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `pengeluaran`
---
-ALTER TABLE `pengeluaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `reseller`
---
-ALTER TABLE `reseller`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pembelian_barang`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `transaksi_dropshipper`
@@ -368,43 +305,39 @@ ALTER TABLE `user`
 -- Constraints for table `gaji`
 --
 ALTER TABLE `gaji`
-  ADD CONSTRAINT `gajidrop` FOREIGN KEY (`id_dropshipper`) REFERENCES `dropshipper` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `gajianuser` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `pemasukan`
+-- Constraints for table `keuntungan_dropshipper`
 --
-ALTER TABLE `pemasukan`
-  ADD CONSTRAINT `fdropshipper` FOREIGN KEY (`id_dropshipper`) REFERENCES `dropshipper` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `freseller` FOREIGN KEY (`id_reseller`) REFERENCES `reseller` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `keuntungan_dropshipper`
+  ADD CONSTRAINT `user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `pembelian`
+-- Constraints for table `pembelian_barang`
 --
-ALTER TABLE `pembelian`
-  ADD CONSTRAINT `id_barang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `packing` FOREIGN KEY (`id_packing`) REFERENCES `packing` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `pengeluaran`
---
-ALTER TABLE `pengeluaran`
-  ADD CONSTRAINT `gaji` FOREIGN KEY (`id_gaji`) REFERENCES `gaji` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `overhead` FOREIGN KEY (`id_overhead`) REFERENCES `overhead` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pembelian` FOREIGN KEY (`id_pembelian`) REFERENCES `pembelian` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `pembelian_barang`
+  ADD CONSTRAINT `barang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `transaksi_dropshipper`
 --
 ALTER TABLE `transaksi_dropshipper`
-  ADD CONSTRAINT `barang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `dropshipper` FOREIGN KEY (`id_dropshipper`) REFERENCES `dropshipper` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tdbarang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tduser` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `transaksi_reseller`
 --
 ALTER TABLE `transaksi_reseller`
-  ADD CONSTRAINT `fbarang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reseller` FOREIGN KEY (`id_reseller`) REFERENCES `reseller` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tbarang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tuser` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `level` FOREIGN KEY (`id_level`) REFERENCES `level` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
